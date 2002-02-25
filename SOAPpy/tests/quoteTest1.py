968abc363c677ffc52da620404061bd2a7469078
gliptak <gliptak@7150bf37-e60d-0410-b93f-83e91ef0e581>
2002-02-25 02:50:41 +0000

    Another quote service test
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@65 7150bf37-e60d-0410-b93f-83e91ef0e581

---
+++ SOAPpy/tests/quoteTest1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+
+sys.path.insert (1, '..')
+
+import SOAP
+
+ident = '$Id$'
+
+# http://www.durrios.com/Finance.wsdl
+
+server = SOAP.SOAPProxy('http://www.durrios.com/soap/finance.cgi',
+    namespace = 'http://www.durrios.com/Finance')
+
+symbol='SUNW'
+print symbol, "-> close ", server.stockquote_lasttrade(symbol=symbol), \
+   " volume ", server.stockquote_volume(symbol=symbol)
