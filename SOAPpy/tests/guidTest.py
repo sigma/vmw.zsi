92c9d900bbe428212ee15e9473b91c8f594b0c64
gliptak <gliptak@7150bf37-e60d-0410-b93f-83e91ef0e581>
2002-02-26 03:25:02 +0000

    Correcting URL
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@70 7150bf37-e60d-0410-b93f-83e91ef0e581

--- SOAPpy/tests/guidTest.py
+++ SOAPpy/tests/guidTest.py
@@ -11,6 +11,6 @@ import SOAP
 ident = '$Id$'
 
 # Three ways to do namespaces, force it at the server level
-server = SOAP.SOAPProxy("http://www.itfinity.net/soap/guid/default.asp",
+server = SOAP.SOAPProxy("http://www.itfinity.net:8008/soap/guid/default.asp",
 		namespace="http://www.itfinity.net/soap/guid/guid.xsd")
 print "GUID>>", server.NextGUID()
