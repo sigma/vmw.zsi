ea10e58ea6b6bbddd6c14605ef8dbf300a60e018
warnes <warnes@7150bf37-e60d-0410-b93f-83e91ef0e581>
2003-03-05 21:43:43 +0000

    - Initial version of setup.py.  Not yet tested!
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@95 7150bf37-e60d-0410-b93f-83e91ef0e581

---
+++ SOAPpy/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+url="http://pywebsvcs.sf.net/"
+
+long_description="SOAPpy provides tools for building SOAP clients and servers.  For more information see " + url
+
+setup(name="SOAPpy",
+      version="0.9.7",
+      description="SOAP Services for Python",
+      author="Cayce Ullman, Brian Matthews, Gregory Warnes, Christopher Blunck"
+      maintainer_email="Gregory Warnes"
+      maintainer_email="gregory_r_warnes@groton.pfizer.com"
+      url = url,
+      long_description=long_description,
+      packages=['SOAPpy',],
+     )
+
