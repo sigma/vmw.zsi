4a995498e9a2b9b38db4dd79154cbc3a5d182900
warnes <warnes@7150bf37-e60d-0410-b93f-83e91ef0e581>
2003-03-12 07:47:59 +0000

    - Updated version number to 0.9.8
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@100 7150bf37-e60d-0410-b93f-83e91ef0e581

--- SOAPpy/setup.py
+++ SOAPpy/setup.py
@@ -5,7 +5,7 @@ url="http://pywebsvcs.sf.net/"
 long_description="SOAPpy provides tools for building SOAP clients and servers.  For more information see " + url
 
 setup(name="SOAPpy",
-      version="0.9.7",
+      version="0.9.8",
       description="SOAP Services for Python",
       author="Cayce Ullman, Brian Matthews, Gregory Warnes, Christopher Blunck",
       maintainer="Gregory Warnes",
