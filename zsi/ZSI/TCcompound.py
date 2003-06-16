f204bc086daca1c1a5495e7e4635690873458723
feanor420 <feanor420@7150bf37-e60d-0410-b93f-83e91ef0e581>
2003-06-16 22:39:00 +0000

    All of the tests work correctly without this line. Neither Rich nor I understood why we would ever be checking for self.pname in the parse method.
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@261 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/ZSI/TCcompound.py
+++ zsi/ZSI/TCcompound.py
@@ -69,8 +69,8 @@ class Struct(TypeCode):
             _check_typecode_list(self.ofwhat, 'Struct')
 
     def parse(self, elt, ps):
-        if elt.localName != self.pname:
-            elt = elt.getElementsByTagName(self.oname)[0]
+        #if elt.localName != self.pname:
+        #    elt = elt.getElementsByTagName(self.pname)[0]
         self.checkname(elt, ps)
         if self.type and \
         self.checktype(elt, ps) not in [ self.type, (None,None) ]:
