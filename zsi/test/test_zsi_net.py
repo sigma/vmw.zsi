bf1ee4536b136202e4150862d9860a368aa4c4d6
feanor420 <feanor420@7150bf37-e60d-0410-b93f-83e91ef0e581>
2003-07-22 01:30:53 +0000

    Modified the README to have slightly more accurate information. I also removed the test that used the http resolver from test_zsi since it requires net access. I then added a test_zsi_net module that will call all of the tests including thouse that require outside connectivity.
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@281 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/test/test_zsi_net.py
+++ zsi/test/test_zsi_net.py
@@ -3,6 +3,7 @@ import unittest
 import test_t1
 import test_t2
 import test_t3
+import test_t4
 import test_t5
 import test_t6
 import test_t7
@@ -11,10 +12,11 @@ def makeTestSuite():
     suite1 = test_t1.makeTestSuite()
     suite2 = test_t2.makeTestSuite()
     suite3 = test_t3.makeTestSuite()
+    suite4 = test_t4.makeTestSuite()
     suite5 = test_t5.makeTestSuite()
     suite6 = test_t6.makeTestSuite()
     suite7 = test_t7.makeTestSuite()
-    t = (suite1, suite2, suite3, suite5, suite6, suite7)
+    t = (suite1, suite2, suite3, suite4, suite5, suite6, suite7)
     suite = unittest.TestSuite(t)
     return suite
 def main():
