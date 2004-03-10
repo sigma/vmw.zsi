a3c249d947327daf6540c2dff0a044f980221dc4
boverhof <boverhof@7150bf37-e60d-0410-b93f-83e91ef0e581>
2004-03-10 02:53:51 +0000

    ----------------------------------------------------------------------
     Modified Files:
     	ServiceProxy.py -- now it supports simple doc/literal stuff too.
    
            I also changed it so all local elements are not namespaced.  This
            is BP-1.0 practice, and it's how the Google response looks anyway.
    
    
    
    RPC/ENCODED GoogleService
    
    <SOAP-ENV:Body>
    <ns1:doSpellingSuggestion xmlns:ns1="urn:GoogleSearch">
    <key id="40347288" xsi:type="xsd:string">hBG+ed8alnS+Ym2JOS+1SBNrpTft+iWi</key>
    <phrase id="40344b38" xsi:type="xsd:string">brtny sprs</phrase>
    </ns1:doSpellingSuggestion>
    </SOAP-ENV:Body>
    </SOAP-ENV:Envelope>
    
    
    <SOAP-ENV:Body>
    <ns1:doSpellingSuggestionResponse xmlns:ns1="urn:GoogleSearch" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
    <return xsi:type="xsd:string">britney spears</return>
    </ns1:doSpellingSuggestionResponse>
    </SOAP-ENV:Body>
    </SOAP-ENV:Envelope>
    
    DOCUMENT/LITERAL
    
        <s:schema elementFormDefault="qualified" targetNamespace="http://www.webserviceX.NET">
          <s:element name="GetWeather">
            <s:complexType>
              <s:sequence>
                <s:element minOccurs="0" maxOccurs="1" name="CityName" type="s:string" />
                <s:element minOccurs="0" maxOccurs="1" name="CountryName" type="s:string" />
              </s:sequence>
            </s:complexType>
          </s:element>
    
    <SOAP-ENV:Body>
    <ns1:GetWeather xmlns:ns1="http://www.webserviceX.NET">
    <CityName id="4035cce0" xsi:type="xsd:string">oakland</CityName>
    <CountryName id="4035cd00" xsi:type="xsd:string">America</CountryName>
    </ns1:GetWeather>
    </SOAP-ENV:Body>
    </SOAP-ENV:Envelope>
    
    
     ----------------------------------------------------------------------
    
    
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@603 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/ZSI/ServiceProxy.py
+++ zsi/ZSI/ServiceProxy.py
@@ -86,9 +86,18 @@ class ServiceProxy:
         return binding.Receive()
 
     def _getTypeCodes(self, callinfo):
-        requestTC = self._getTypeCode(parameters=callinfo.getInParameters())
-        replyTC = self._getTypeCode(parameters=callinfo.getOutParameters())
+        """Returns typecodes representing input and output messages
+           callinfo --  WSDLTools.SOAPCallInfo instance describing an operation.
+        """
+        prefix = None
+        self._resetPrefixDict()
+        if callinfo.use == 'encoded':
+            prefix = self._getPrefix(callinfo.namespace)
+        requestTC = self._getTypeCode(parameters=callinfo.getInParameters(), literal=(callinfo.use=='literal'))
 
+        self._resetPrefixDict()
+        replyTC = self._getTypeCode(parameters=callinfo.getOutParameters(), literal=(callinfo.use=='literal'))
+        
         if callinfo.style == 'rpc':
             request = TC.Struct(pyclass=None, ofwhat=requestTC, pname=callinfo.methodName)
             response = TC.Struct(pyclass=None, ofwhat=replyTC, pname='%sResponse' %callinfo.methodName)
@@ -96,51 +105,123 @@ class ServiceProxy:
             request = requestTC[0]
             response = replyTC[0]
 
-        if callinfo.use == 'encoded':
-            request.oname += ' xmlns="%s"' %callinfo.namespace
+        #THIS IS FOR RPC/ENCODED, DOC/ENCODED Wrapper
+        if prefix and callinfo.use == 'encoded':
+            request.oname = '%(prefix)s:%(name)s xmlns:%(prefix)s="%(namespaceURI)s"' \
+                %{'prefix':prefix, 'name':request.oname, 'namespaceURI':callinfo.namespace}
 
         return request, response
 
-    def _getTypeCode(self, parameters):
-        bti = BaseTypeInterpreter()
-        typeCode = []
+    def _getTypeCode(self, parameters, literal=False):
+        """Returns typecodes representing a parameter set
+           parameters -- list of WSDLTools.ParameterInfo instances representing
+              the parts of a WSDL Message.
+        """
+        ofwhat = []
         for part in parameters:
             namespaceURI,localName = part.type
 
             if part.element_type:
                 #global element
-                element = self._wsdl.types[nsuri].elements[localName]
-                name = element.attributes['name']
-                tdc = element.attributes['element']
-                typeClass = bti.get_typeclass(msg_type=tdc.getName(),
-                                              targetNamespace=tdc.getTargetNamespace())
-                tc = typeClass(name)
-                if not typeClass:
-                    element = self._wsdl.types[tdc.getTargetNamespace()].getElementDeclaration(tdc.getName())
-                    tc = self._getElement(element)
-                else:
-                    tc = typeClass(name)
-
+                element = self._wsdl.types[namespaceURI].elements[localName]
+                tc = self._getElement(element, literal=literal, local=False, namespaceURI=namespaceURI)
             else:
                 #local element
                 name = part.name
-                typeClass = bti.get_typeclass(msg_type=localName, targetNamespace=namespaceURI)
+                typeClass = self._getTypeClass(namespaceURI, localName)
                 if not typeClass:
                     self._wsdl.types[namespaceURI].getTypeDefinition(localName)
                     typeClass = self._getElement()
                 tc = typeClass(name)
-            typeCode.append(tc)
-        return typeCode
+            ofwhat.append(tc)
+        return ofwhat
 
-    def _getElement(self, element):
-        name = element.getAttribute('name')
+    def _globalElement(self, typeCode, namespaceURI, literal):
+        """namespaces typecodes representing global elements with 
+             literal encoding.
+           typeCode -- typecode representing an element.
+           namespaceURI -- namespace 
+           literal -- True/False
+        """
+        if literal:
+            typeCode.oname = '%(prefix)s:%(name)s xmlns:%(prefix)s="%(namespaceURI)s"' \
+                %{'prefix':self._getPrefix(namespaceURI), 'name':typeCode.oname, 'namespaceURI':namespaceURI}
+
+    def _getPrefix(self, namespaceURI):
+        """Retrieves a prefix/namespace mapping.
+           namespaceURI -- namespace 
+        """
+        prefixDict = self._getPrefixDict()
+        if prefixDict.has_key(namespaceURI):
+            prefix = prefixDict[namespaceURI]
+        else:
+            prefix = 'ns1'
+            while prefix in prefixDict.values():
+                prefix = 'ns%d' %int(prefix[-1]) + 1
+            prefixDict[namespaceURI] = prefix
+        return prefix
+
+    def _getPrefixDict(self):
+        """Used to hide the actual prefix dictionary.
+        """
+        if not hasattr(self, '_prefixDict'):
+            self.__prefixDict = {}
+        return self.__prefixDict
+
+    def _resetPrefixDict(self):
+        """Clears the prefix dictionary, this needs to be done 
+           before creating a new typecode for a message 
+           (ie. before, and after creating a new message typecode)
+        """
+        self._getPrefixDict().clear()
+
+    def _getElement(self, element, literal=False, local=False, namespaceURI=None):
+        """Returns a typecode instance representing the passed in element.
+           element -- XMLSchema.ElementDeclaration instance
+           literal -- literal encoding?
+           local -- is locally defined?
+           namespaceURI -- namespace
+        """
+        if not element.isElement():
+            raise TypeError, 'Expecting an ElementDeclaration'
+
+        tc = None
+        ofwhat = []
+
+        elementName = element.getAttribute('name')
         tp = element.getTypeDefinition('type')
-        if type(tp) in (types.StringType, types.Unicode):
-            pass
-        return self._getType(self, name, tp)
+
+        if not (tp or element.content):
+            nsuriType,localName = element.getAttribute('type')
+            minOccurs = element.getAttribute('minOccurs')
+            maxOccurs = element.getAttribute('maxOccurs')
+            typeClass = self._getTypeClass(nsuriType,localName)
+            return typeClass(elementName, repeatable=maxOccurs>1, optional=not minOccurs)
+        elif not tp:
+            tp = element.content
+
+        if not (tp.isDefinition() and tp.isComplex()):
+            raise EvaluationError, 'only supporting complexType definition'
+        elif not tp.content.isModelGroup():
+            raise EvaluationError, 'only supporting a model group, no derivations'
+
+        modelGroup = tp.content
+        for item in modelGroup.content:
+            ofwhat.append(self._getElement(item, literal=literal, local=True))
+
+        tc = TC.Struct(pyclass=None, ofwhat=ofwhat, pname=elementName)
+        if not local:
+            self._globalElement(tc, namespaceURI=namespaceURI, literal=literal)
+        return tc
     
-    def _getType(self, name, tp):
-        return        
+    def _getTypeClass(self, namespaceURI, localName):
+        """Returns a typecode class representing the type we are looking for.
+           localName -- name of the type we are looking for.
+           namespaceURI -- defining XMLSchema targetNamespace.
+        """
+        bti = BaseTypeInterpreter()
+        simpleTypeClass = bti.get_typeclass(localName, namespaceURI)
+        return simpleTypeClass
 
 
 class MethodProxy:
