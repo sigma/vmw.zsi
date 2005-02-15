0accda952f37aa561ffa057673622333f8b4fbb1
warnes <warnes@7150bf37-e60d-0410-b93f-83e91ef0e581>
2005-02-15 20:15:55 +0000

    Fix [ 925077 ] SOAPpy prints out SOAP fault /even when Config.debug is off/.
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@932 7150bf37-e60d-0410-b93f-83e91ef0e581

--- SOAPpy/SOAPpy/Client.py
+++ SOAPpy/SOAPpy/Client.py
@@ -385,7 +385,8 @@ class SOAPProxy:
             throw_struct = 0
 
         if throw_struct:
-            print p
+            if Config.debug:
+                print p
             raise p
 
         # If unwrap_results=1 and there is only element in the struct,
