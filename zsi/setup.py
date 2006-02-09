8ed6d69c887d20138d9f01cd6d5884a5514e7cca
boverhof <boverhof@7150bf37-e60d-0410-b93f-83e91ef0e581>
2006-02-09 21:42:45 +0000

    ----------------------------------------------------------------------
     Modified Files:
     	setup.py --- ZSI.twisted is an "optional" build.
    
    	To use:
                python setup.py build --twisted
                python setup.py install
    
     ----------------------------------------------------------------------
    
    
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@1128 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/setup.py
+++ zsi/setup.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 # $Header$
 import sys
+from distutils.command.build import build as _build
+from distutils.command.build_py import build_py as _build_py
 try:
     from setuptools import setup
 except ImportError:
@@ -28,11 +30,46 @@ except:
     print 'ZSI/version.py not found; run "make"'
     sys.exit(1)
 
+_twisted_packages = ['ZSI.twisted']
+_twisted_options = [('twisted', None, 'build twisted packages')]
+
+class build_py(_build_py):
+    user_options = _build_py.user_options[:]
+    user_options += _twisted_options
+    boolean_options = _build.boolean_options[:]
+    boolean_options.append(_twisted_options[0][0])
+
+    def initialize_options(self):
+        _build_py.initialize_options(self)
+        self.twisted = False
+
+    def run(self):
+        if self.twisted:
+            self.distribution.packages += _twisted_packages
+        return _build_py.run(self)
+
+
+class build(_build):
+    user_options = _build.user_options[:]
+    user_options += _twisted_options
+    boolean_options = _build.boolean_options[:]
+    boolean_options.append(_twisted_options[0][0])
+
+    def initialize_options(self):
+        _build.initialize_options(self)
+        self.twisted = False
+
+    def run(self):
+        if self.twisted:
+            self.distribution.packages += _twisted_packages
+        return _build.run(self)
+
+
 setup(
     name="ZSI",
     version=_version,
     license="Python",
-    packages=[ "ZSI", "ZSI.generate", "ZSI.twisted", "ZSI.wstools"],
+    packages=[ "ZSI", "ZSI.generate", "ZSI.wstools"],
     scripts=["scripts/wsdl2py.py", "scripts/wsdl2dispatch.py"],
     description="Zolera SOAP Infrastructure",
     author="Rich Salz, et al",
@@ -41,4 +78,5 @@ setup(
     maintainer_email="pywebsvcs-talk@lists.sf.net",
     url=_url,
     long_description='For additional information, please see ' + _url,
+    cmdclass={'build':build, 'build_py':build_py},
 )
