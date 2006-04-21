6405386acc3301b2a83ddeb73205fd071e9120bb
boverhof <boverhof@7150bf37-e60d-0410-b93f-83e91ef0e581>
2006-04-21 20:49:22 +0000

    M    setup.py M    README M    CHANGES 	-If Python >= 2.4, build/install ZSI.twisted package          else don't add to packages.
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@1179 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/setup.py
+++ zsi/setup.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python
 # $Header$
 import sys
-from distutils.command.build import build as _build
-from distutils.command.build_py import build_py as _build_py
 try:
     from setuptools import setup
 except ImportError:
@@ -30,46 +28,15 @@ except:
     print 'ZSI/version.py not found; run "make"'
     sys.exit(1)
 
-_twisted_packages = ['ZSI.twisted']
-_twisted_options = [('twisted', None, 'build twisted packages')]
-
-class build_py(_build_py):
-    user_options = _build_py.user_options[:]
-    user_options += _twisted_options
-    boolean_options = _build.boolean_options[:]
-    boolean_options.append(_twisted_options[0][0])
-
-    def initialize_options(self):
-        _build_py.initialize_options(self)
-        self.twisted = False
-
-    def run(self):
-        if self.twisted:
-            self.distribution.packages += _twisted_packages
-        return _build_py.run(self)
-
-
-class build(_build):
-    user_options = _build.user_options[:]
-    user_options += _twisted_options
-    boolean_options = _build.boolean_options[:]
-    boolean_options.append(_twisted_options[0][0])
-
-    def initialize_options(self):
-        _build.initialize_options(self)
-        self.twisted = False
-
-    def run(self):
-        if self.twisted:
-            self.distribution.packages += _twisted_packages
-        return _build.run(self)
-
+_packages = [ "ZSI", "ZSI.generate", "ZSI.wstools"]
+if sys.version_info[0:2] >= (2, 4):
+    _packages.append("ZSI.twisted")
 
 setup(
     name="ZSI",
     version=_version,
     license="Python",
-    packages=[ "ZSI", "ZSI.generate", "ZSI.wstools"],
+    packages=_packages,
     scripts=["scripts/wsdl2py.py", "scripts/wsdl2dispatch.py"],
     description="Zolera SOAP Infrastructure",
     author="Rich Salz, et al",
@@ -77,6 +44,5 @@ setup(
     maintainer="Rich Salz, et al",
     maintainer_email="pywebsvcs-talk@lists.sf.net",
     url=_url,
-    long_description='For additional information, please see ' + _url,
-    cmdclass={'build':build, 'build_py':build_py},
+    long_description="For additional information, please see " + _url,
 )
