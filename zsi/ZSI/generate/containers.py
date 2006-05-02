sigma/vmw.zsi

commit 09b71392ead57c09b640606edfb021fb6ecbbf84

A    test/wsdl2py/test_Choice.py M    test/wsdl2py/config.txt A    test/wsdl2py/test_Choice.xsd M    ZSI/generate/containers.py
Added unittests and bug fix for [ 1441574 ] ZSI assumes minOccurs(1) for all parts.




git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@1153 7150bf37-e60d-0410-b93f-83e91ef0e581

zsi/ZSI/generate/containers.py
@@ -1279,82 +1279,139 @@ class TypecodeContainerBase(TypesContainerBase):
         else:
             return 'def __init__(self, pname, **kw):'
 
-    # _flattenContent() is a pre-processor called before either the
-    # elements or typecode strings are generated.  it flattens out
-    # embedded/anonymous model group elements.  _setElements() and
-    # _setTypecodeList() skip the model groups and pay attention
-    # to the elements contained therein.
-
-    def _flattenContent(self):
-        if not self.contentFlattened:
-            self.contentFlattened = True
-            if type(self.mgContent) is not tuple:
-                raise TypeError, 'expecting tuple for mgContent, not: %s' %type(self.mgContent)
-            for c in self.mgContent:
-                if c.isChoice() or c.isSequence():
-                    self.mgContent += c.content
-
-    # getElements() and _setElements() generate the class variable
-    # elements lists in definition classes
-
     def _setElements(self):
+        """This method ONLY sets up the instance attributes.
+        """
+        flattened = []
+        content = self.mgContent
         if type(self.mgContent) is not tuple:
-            raise TypeError, 'expecting tuple for mgContent got %s' %type(self.mgContent)
+            content = self.mgContent.content
+            if self.mgContent.isAll():
+                flattend = content
+                content = []
 
-        for c in self.mgContent:
+        idx = 0
+        content = list(content)
+        while idx < len(content):
+            c = content[idx]
+            if not c.isModelGroup():
+                flattened.append(c)
+                idx += 1
+                continue
+                
+            if c.isSequence() or c.isChoice():
+                begIdx = idx
+                endIdx = begIdx + len(c.content)
+                for i in range(begIdx, endIdx):
+                    content.insert(i, c.content[i-begIdx])
+                    
+                content.remove(c)
+                continue
+            
+            raise ContainerError, 'unexpected schema item: %s' %c.getItemTrace()
+                
+        for c in flattened:
             if c.isDeclaration() and c.isElement():
-                if c.getAttribute('maxOccurs') == 'unbounded':
-                    defaultValue = "[]"
-                else:
-                    defaultValue = "None"
-                if c.getAttribute('name'):
-                    e = '%sself.%s = %s' \
-                        %(ID3, self.getAttributeName(c.getAttribute('name')), defaultValue)
-                    self.elementAttrs.append(e)
-                elif c.isWildCard():
-                    e = '%sself.%s = %s' % (ID3, self.getAttributeName(c.getAttribute('name')), defaultValue)
-                else:
-                    raise ContainerError, 'This is not an element declaration: %s' %c.getItemTrace()
-            elif c.isReference():
-                if c.getAttribute('ref')[1]:
-                    e = '%sself._%s = None' \
-                        % (ID3,self.mangle(c.getAttribute('ref')[1]))
-                    self.elementAttrs.append(e)
-                else:
-                    raise ContainerError, 'ouch as well'
-            elif c.isSequence() or c.isChoice():
-                if not self.contentFlattened:
-                    raise ContainerError, 'unflattened content found!'
-                else:
-                    # model group contents already flattened
-                    pass
-            else:
-                raise ContainerError, 'unexpected item: %s' % c.getItemTrace()
+                defaultValue = "None"
+                parent = c
+                while 1:
+                    maxOccurs = parent.getAttribute('maxOccurs')
+                    if maxOccurs == 'unbounded' or int(maxOccurs) > 1:
+                        defaultValue = "[]"
+                        break
+                    
+                    parent = parent._parent()
+                    if not parent.isModelGroup():
+                        break
+                    
+                e = '%sself.%s = %s' %(ID3, 
+                        self.getAttributeName(c.getAttribute('name')), defaultValue)
+                self.elementAttrs.append(e)
+                continue
+                        
+            # TODO: This seems wrong 
+            if c.isReference():
+                e = '%sself._%s = None' %(ID3,
+                        self.mangle(c.getAttribute('ref')[1]))
+                self.elementAttrs.append(e)
+                continue
+            
+            raise ContainerError, 'unexpected item: %s' % c.getItemTrace()
 
     def getElements(self):
         if not self.elementsSet:
-            self._flattenContent()
+#            self._flattenContent()
             self._setElements()
             self.elementsSet = True
 
         return '\n'.join(self.elementAttrs)
 
     def _setTypecodeList(self):
-        '''getTypecodeList and _setTypecodeList generates the TClist 
-        strings where appropriate.
-
+        '''generates ofwhat content, minOccurs/maxOccurs facet generation.
             mgContent -- 
             localTypes -- produce local class definitions later
             tcListElements -- elements, local/global 
- 
         '''
+        flattened = []
+        content = self.mgContent
         if type(self.mgContent) is not tuple:
-            raise TypeError, 'expecting tuple for mgContent got %s' %type(self.mgContent)
+            content = self.mgContent.content
+            if self.mgContent.isAll():
+                flattend = content
+                content = []
+    
+        idx = 0
+        content = list(content)
+        while idx < len(content):
+            c = content[idx]
+            if not c.isModelGroup():
+                flattened.append(c)
+                idx += 1
+                continue
+                
+            if c.isSequence() or c.isChoice():
+                begIdx = idx
+                endIdx = begIdx + len(c.content)
+                for i in range(begIdx, endIdx):
+                    content.insert(i, c.content[i-begIdx])
+                    
+                content.remove(c)
+                continue
+            
+            raise ContainerError, 'unexpected schema item: %s' %c.getItemTrace()
 
-        for c in self.mgContent:
+        for c in flattened:
             tc = TcListComponentContainer()
+            # TODO: Remove _getOccurs
             min,max,nil = self._getOccurs(c)
-            tc.setOccurs(min, max, nil)
+            min = max = None
+            maxOccurs = 1
+            
+            parent = c
+            while 1:
+                max = parent.getAttribute('maxOccurs')
+                if max == 'unbounded':
+                    maxOccurs = '"%s"' %max 
+                    break
+                
+                maxOccurs = int(max) * maxOccurs
+                parent = parent._parent()
+                if not parent.isModelGroup():
+                    break
+                
+            parent = c
+            while 1:
+                minOccurs = int(parent.getAttribute('minOccurs'))
+                if minOccurs == 0 or parent.isChoice():
+                    minOccurs = 0
+                    break
+                
+                parent = parent._parent()
+                if not parent.isModelGroup():
+                    minOccurs = int(c.getAttribute('minOccurs'))
+                    break
+            
+            tc.setOccurs(minOccurs, maxOccurs, nil)
             processContents = self._getProcessContents(c)
             tc.setProcessContents(processContents)
 
@@ -1400,10 +1457,6 @@ class TypecodeContainerBase(TypesContainerBase):
                 tc.klass = '%s.%s' % (NAD.getAlias(ns),
                                           element_class_name(c.getAttribute('ref')[1]) )
                 tc.setStyleElementReference()
-            elif c.isSequence() or c.isChoice():
-                if not self.contentFlattened:
-                    raise ContainerError, 'unflattened content found!'
-                continue
             else:
                 raise ContainerError, 'unexpected item: %s' % c.getItemTrace()
 
@@ -1411,7 +1464,7 @@ class TypecodeContainerBase(TypesContainerBase):
 
     def getTypecodeList(self):
         if not self.tcListSet:
-            self._flattenContent()
+#            self._flattenContent()
             self._setTypecodeList()
             self.tcListSet = True
 
@@ -1541,7 +1594,7 @@ class MessageTypecodeContainer(TypecodeContainerBase):
     def setParts(self, parts):
         self.mgContent = parts
 
-#class TcListComponentContainer(TypecodeContainerBase):
+
 class TcListComponentContainer(ContainerBase):
     '''Encapsulates a single value in the TClist list.
     it inherits TypecodeContainerBase only to get the mangle() method,
@@ -2153,18 +2206,26 @@ class ComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
         self.ns = tp.getTargetNamespace()
         self.mixed = tp.isMixed()
         self.mgContent = ()
-        
-        if empty is True or tp.content is None:
-            pass
-
-        elif tp.content.isReference() is True: 
-            ref = tp.content.getModelGroupReference()
-            if ref.content.content is not None:
-               self.mgContent = ref.content.content
-        elif tp.content.content is not None:
-            self.mgContent = tp.content.content
-
         self.attrComponents = self._setAttributes(tp.getAttributeContent())
+        
+        if empty:
+            return
+        
+        model = tp.content
+        if model.isReference(): 
+            model = model.getModelGroupReference()
+        
+        if model is None:
+            return
+        
+        if model.content is None:
+            return
+       
+        # sequence, all or choice
+        #self.mgContent = model.content
+        self.mgContent = model
+
+        
         
     def _setContent(self):
         definition = [
@@ -2178,7 +2239,8 @@ class ComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
             '%sTClist = [%s]' % (ID3, self.getTypecodeList()),
             ]
 
-        definition.append('%s%s = attributes or {}' %(ID3, self.attribute_typecode))
+        definition.append('%s%s = attributes or {}' %(ID3, 
+                           self.attribute_typecode))
         # IF EXTEND
         definition.append('%sif extend: TClist += ofwhat'%(ID3))
         # IF RESTRICT

zsi/test/wsdl2py/test_Choice.py
@@ -1,0 +1,71 @@
+#!/usr/bin/env python
+############################################################################
+# Joshua R. Boverhof, LBNL
+# See LBNLCopyright for copyright notice!
+###########################################################################
+import os, sys, unittest
+from ServiceTest import ServiceTestCase, ServiceTestSuite
+from ZSI import FaultException
+from ZSI.TC import _get_global_element_declaration as GED
+from ZSI.writer import SoapWriter
+
+"""
+Unittest for Bug Report 
+[ 1441574 ] ZSI assumes minOccurs(1) for all parts
+
+WSDL:  
+"""
+
+CONFIG_FILE = 'config.txt'
+sys.path.append('%s/%s' %(os.getcwd(), 'stubs'))
+
+class ChoiceTest(unittest.TestCase):
+    name = "test_Choice"
+    done = False
+
+    def setUp(self):
+        if ChoiceTest.done: return
+        ChoiceTest.done = True
+        wsdl = os.path.abspath('test_Choice.xsd').strip()
+        fin,fout = os.popen4('cd stubs; wsdl2py.py -x -b -f %s' %wsdl)
+        # TODO: wait for wsdl2py.py to finish.
+        for i in fout: print i
+        import test_Choice_xsd_services_types
+ 
+    def tearDown(self):
+        pass
+
+    def test_choice_default_facets_legal1(self):
+        """<choice minOccurs=1 maxOccurs=1>
+        """
+        pyobj = GED("urn:example", "Easy").pyclass()
+        pyobj.Rank = 1
+        sw = SoapWriter()
+        sw.serialize(pyobj)
+        print str(sw)
+
+    def test_choice_maxOccurs_unbounded(self):
+        """<choice minOccurs=1 maxOccurs=unbounded>
+        """
+        pyobj = GED("urn:example", "Hard").pyclass()
+        pyobj.Name = "steve"
+        pyobj.Name.append("mark")
+        pyobj.Any = "whatever"
+        pyobj.Rank = 2
+        pyobj.Rank.append(3)
+        pyobj.Rank.append(4)
+        sw = SoapWriter()
+        sw.serialize(pyobj)
+        print str(sw)
+
+
+
+def makeTestSuite():
+    suite = ServiceTestSuite()
+    suite.addTest(unittest.makeSuite(ChoiceTest, "test_", suiteClass=ServiceTestSuite))
+    return suite
+
+
+if __name__ == "__main__" :
+    unittest.TestProgram(defaultTest="makeTestSuite")
+

zsi/test/wsdl2py/test_Choice.xsd
@@ -1,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
+    targetNamespace="urn:example"
+    xmlns:bt="urn:example">
+
+<xs:element name="Easy" type="bt:EasyChoice"/>
+<xs:element name="Hard" type="bt:HardChoice"/>
+
+<xs:complexType name="EasyChoice">
+  <xs:choice>
+    <xs:element name="name" type="xs:string"/>
+    <xs:element name="rank" type="xs:int"/>
+    <xs:element name="any"/>
+  </xs:choice>
+</xs:complexType>  
+
+<xs:complexType name="HardChoice">
+  <xs:choice maxOccurs="unbounded">
+    <xs:element name="name" type="xs:string"/>
+    <xs:element name="rank" type="xs:int"/>
+    <xs:element name="any"/>
+  </xs:choice>
+</xs:complexType>  
+
+</xs:schema>

commit 5a63ff860ff4ca34a4be7e3546b2601faf89cfd7

M    generate/containers.py
Fixed typo introduced in revision:

    r1153 | boverhof | 2006-03-13 22:47:20 -0800 (Mon, 13 Mar 2006) | 10 lines

Also added a bunch of logger.debug statements to aid debugging of complexTypes,
also added a "_item" attribute in all schema "setUp" methods.  This can be used
to display the schema item in debug or exceptions.

for example:

print self._item.getItemTrace()
<schema targetNamespace="http://www.ebi.ac.uk/WSWUBlast"><complexType name="WSFile">



git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@1197 7150bf37-e60d-0410-b93f-83e91ef0e581

zsi/ZSI/generate/containers.py
@@ -119,7 +119,7 @@ class AttributeMixIn:
     
     def _setAttributes(self, attributes):
         '''parameters
-        attributes -- a flattened list of all attributes, 
+        attributes -- a flat list of all attributes, 
         from this list all items in attribute_typecode_dict will
         be generated into attrComponents.
         
@@ -1294,13 +1294,13 @@ class TypecodeContainerBase(TypesContainerBase):
                 with model group content, a model group, or model group 
                 content.  TODO: should only support the first two.
         """
-
+        self.logger.debug("_setUpElements: %s" %self._item.getItemTrace())
         if hasattr(self, '_done'):
             #return '\n'.join(self.elementAttrs)
             return
         
         self._done = True
-        flattened = []
+        flat = []
         content = self.mgContent
         if type(self.mgContent) is not tuple:
             mg = self.mgContent
@@ -1309,7 +1309,7 @@ class TypecodeContainerBase(TypesContainerBase):
                 
             content = mg.content
             if mg.isAll():
-                flattend = content
+                flat = content
                 content = [] 
             elif mg.isModelGroup() and mg.isDefinition():
                 mg = mg.content
@@ -1320,7 +1320,7 @@ class TypecodeContainerBase(TypesContainerBase):
         while idx < len(content):
             c = orig = content[idx]
             if c.isElement():
-                flattened.append(c)
+                flat.append(c)
                 idx += 1
                 continue
             
@@ -1341,7 +1341,7 @@ class TypecodeContainerBase(TypesContainerBase):
             
             raise ContainerError, 'unexpected schema item: %s' %c.getItemTrace()
                 
-        for c in flattened:
+        for c in flat:
             if c.isDeclaration() and c.isElement():
                 defaultValue = "None"
                 parent = c
@@ -1394,16 +1394,25 @@ class TypecodeContainerBase(TypesContainerBase):
             localTypes -- produce local class definitions later
             tcListElements -- elements, local/global 
         """
-        flattened = []
+        self.logger.debug("_setTypecodeList(%r): %s" %
+                          (self.mgContent, self._item.getItemTrace()))
+        
+        flat = []
         content = self.mgContent
+        
+        #TODO: too much slop permitted here, impossible
+        # to tell what is going on.
         if type(self.mgContent) is not tuple:
             mg = self.mgContent
-            if not mg.isModelGroup(): 
+            if not mg.isModelGroup():
                 mg = mg.content
                 
-            content = mg.content
+            self.logger.debug("ModelGroup(%r) contents(%r): %s" %
+                  (mg, mg.content, self._item.getItemTrace()))
+            
+            #content = mg.content
             if mg.isAll():
-                flattend = content
+                flat = mg.content
                 content = [] 
             elif mg.isModelGroup() and mg.isDefinition():
                 mg = mg.content
@@ -1411,10 +1420,11 @@ class TypecodeContainerBase(TypesContainerBase):
                 
         idx = 0
         content = list(content)
+        self.logger.debug("content: %r" %content)
         while idx < len(content):
             c = orig = content[idx]
             if c.isElement():
-                flattened.append(c)
+                flat.append(c)
                 idx += 1
                 continue
             
@@ -1439,7 +1449,8 @@ class TypecodeContainerBase(TypesContainerBase):
         #    because cannot follow references, but not currently
         #    a big concern. 
         
-        for c in flattened:
+        self.logger.debug("flat: %r" %flat)
+        for c in flat:
             tc = TcListComponentContainer()
             # TODO: Remove _getOccurs
             min,max,nil = self._getOccurs(c)
@@ -1624,6 +1635,9 @@ class MessageTypecodeContainer(TypecodeContainerBase):
         return minOccurs,maxOccurs,nillable
 
     def _setTypecodeList(self):
+        self.logger.debug("MessageTypecodeContainer._setTypecodeList: %s" %
+            str(self.mgContent))
+        
         assert type(self.mgContent) is tuple,\
             'expecting tuple for mgContent not: %s' %type(self.mgContent)
 
@@ -1802,6 +1816,7 @@ class ElementSimpleTypeContainer(TypecodeContainerBase):
     type = DEC
 
     def setUp(self, tp):
+        self._item = tp
         try:
             self.name = tp.getAttribute('name')
             self.ns = tp.getTargetNamespace()
@@ -1865,6 +1880,7 @@ class ElementLocalSimpleTypeContainer(TypecodeContainerBase):
         self.writeArray(element)
 
     def setUp(self, tp):
+        self._item = tp
         assert tp.isElement() is True and tp.content is not None and \
             tp.content.isLocal() is True and tp.content.isSimple() is True ,\
             'expecting local simple type: %s' %tp.getItemTrace()
@@ -1918,6 +1934,7 @@ class ElementLocalComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
         'group', 'all', 'choice', 'sequence', 'attribute', 'attributeGroup',\
         'anyAttribute', 'any']}
         '''
+        self._item = tp
         # JRB HACK SUPPORTING element/no content.
         assert tp.isElement() is True and \
             (tp.content is None or (tp.content.isComplex() is True and tp.content.isLocal() is True)),\
@@ -2010,7 +2027,7 @@ class ElementGlobalDefContainer(TypecodeContainerBase):
 
     def setUp(self, element):
         # Save for debugging
-        self._element = element
+        self._item = element
         
         self.name = element.getAttribute('name')
         self.ns = element.getTargetNamespace()
@@ -2036,7 +2053,7 @@ class ElementGlobalDefContainer(TypecodeContainerBase):
                 '%sif self.pyclass is not None: self.pyclass.__name__ = "%s_Holder"' %(ID3, self.getClassName()),
                 ]
         except Exception, ex:
-            args = ['Failure processing: %s' %self._element.getItemTrace()]
+            args = ['Failure processing: %s' %self._item.getItemTrace()]
             args += ex.args
             ex.args = tuple(args)
             raise
@@ -2058,6 +2075,7 @@ class ComplexTypeComplexContentContainer(TypecodeContainerBase, AttributeMixIn):
             extension
             extType -- used in figuring attrs for extensions
         '''
+        self._item = tp
         assert tp.content.isComplex() is True and \
             (tp.content.content.isRestriction() or tp.content.content.isExtension() is True),\
             'expecting complexContent/[extension,restriction]'
@@ -2292,6 +2310,8 @@ class ComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
            tp -- type definition
            empty -- no model group, just use as a dummy holder.
         '''
+        self._item = tp
+        
         self.name = tp.getAttribute('name')
         self.ns = tp.getTargetNamespace()
         self.mixed = tp.isMixed()
@@ -2299,7 +2319,7 @@ class ComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
         self.attrComponents = self._setAttributes(tp.getAttributeContent())
         
         # Save reference to type for debugging
-        self._tp = tp
+        self._item = tp
         
         if empty:
             return
@@ -2331,7 +2351,7 @@ class ComplexTypeContainer(TypecodeContainerBase, AttributeMixIn):
                 '%sTClist = [%s]' % (ID3, self.getTypecodeList()),
                 ]
         except Exception, ex:
-            args = ["Failure processing %s" %self._tp.getItemTrace()]
+            args = ["Failure processing %s" %self._item.getItemTrace()]
             args += ex.args
             ex.args = tuple(args)
             raise
@@ -2415,6 +2435,8 @@ class RestrictionContainer(SimpleTypeContainer):
     '''
 
     def setUp(self, tp):
+        self._item = tp
+        
         assert tp.isSimple() is True and tp.isDefinition() is True and \
             tp.content.isRestriction() is True,\
             'expecting simpleType restriction, not: %s' %tp.getItemTrace()
@@ -2512,6 +2534,8 @@ class ComplexTypeSimpleContentContainer(SimpleTypeContainer, AttributeMixIn):
     def setUp(self, tp):
         '''tp -- complexType/simpleContent/[Exention,Restriction]
         '''
+        self._item = tp
+        
         assert tp.isComplex() is True and tp.content.isSimple() is True,\
             'expecting complexType/simpleContent not: %s' %tp.content.getItemTrace()
 
@@ -2610,6 +2634,8 @@ class UnionContainer(SimpleTypeContainer):
         self.memberTypes = None
 
     def setUp(self, tp):
+        self._item = tp
+        
         if tp.content.isUnion() is False:
             raise ContainerError, 'content must be a Union: %s' %tp.getItemTrace()
         self.name = tp.getAttribute('name')
@@ -2638,6 +2664,8 @@ class ListContainer(SimpleTypeContainer):
     type = DEF
 
     def setUp(self, tp):
+        self._item = tp
+        
         if tp.content.isList() is False:
             raise ContainerError, 'content must be a List: %s' %tp.getItemTrace()
         self.name = tp.getAttribute('name')