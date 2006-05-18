defa96701adf00f797a9c2d099185200860f2561
boverhof <boverhof@7150bf37-e60d-0410-b93f-83e91ef0e581>
2006-05-18 05:44:59 +0000

    M    test/wsdl2py/test_DerivedTypes.py M    test/wsdl2py/test_DerivedTypes.xsd
        another unittest for 
    	
            # [ 1489129 ] Unexpected subsitution error message
            # [ 1489090 ] Derived type attributes don't populate the attr dictionary
            # [ 1489677 ] Derivation from derived type missing derived element
            # 
    
    
        tests for case when maxOccurs>1
    
    
    git-svn-id: https://pywebsvcs.svn.sourceforge.net/svnroot/pywebsvcs/trunk@1219 7150bf37-e60d-0410-b93f-83e91ef0e581

--- zsi/test/wsdl2py/test_DerivedTypes.py
+++ zsi/test/wsdl2py/test_DerivedTypes.py
@@ -69,6 +69,52 @@ class DTTestCase(ServiceTestCase):
         sw = SoapWriter()
         self.failUnlessRaises(TypeError, sw.serialize, pyobj)
         
+    def test_local_type_substitution_test2(self):
+        """test extension of extension"""
+
+        attr1 = 'aone'
+        attr2 = 'atwo'
+        attr3 = 'athree'
+        self.types_module
+        pyobj = GED('urn:test', 'test2').pyclass()
+
+        # Test maxOccurs>1 for substitution 
+        # 
+        pyobj.Actor = GTD('urn:test', 'TopActor')(None).pyclass()
+        sub1 = pyobj.Actor[0]
+        sub1.Element1 = 'one'
+        sub1.Element2 = 'two'
+        sub1.Element3 = 'three'
+        sub1.set_attribute_attr1(attr1)
+        sub1.set_attribute_attr2(attr2)
+        sub1.set_attribute_attr3(attr3)
+        
+        sw = SoapWriter()
+        sw.serialize(pyobj)
+        xml = str(sw)
+        ps = ParsedSoap(xml)
+        pyobj2 = ps.Parse(pyobj.typecode)
+        sub2 = pyobj2.Actor[0]
+
+        self.failUnless(sub2.get_attribute_attr1() == attr1, 'bad attribute 1')
+        self.failUnless(sub2.get_attribute_attr2() == attr2, 'bad attribute 2')
+        self.failUnless(sub2.get_attribute_attr3() == attr3, 'bad attribute 3')
+
+        self.failUnless(sub2.Element1 == sub1.Element1, 'bad element 1')
+        self.failUnless(sub2.Element2 == sub1.Element2, 'bad element 2')
+        self.failUnless(sub2.Element3 == sub1.Element3, 'bad element 3')
+                
+        # check parsed out correct type
+        self.failUnless(isinstance(sub2.typecode, sub1.typecode.__class__), 
+            'local element actor "%s" must be an instance of "%s"'%
+                (sub2.typecode, sub1.typecode.__class__))
+        
+        # check local element is derived from base
+        base = GTD('urn:test', 'BaseActor')
+        self.failUnless(isinstance(sub2.typecode, base), 
+            'local element actor must be a derived type of "%s"'%
+                base)
+
         
     def test_local_type_substitution2(self):
         """test extension of extension"""
@@ -103,7 +149,6 @@ class DTTestCase(ServiceTestCase):
         sw = SoapWriter()
         sw.serialize(pyobj)
         xml = str(sw)
-        print xml        
         ps = ParsedSoap(xml)
         pyobj2 = ps.Parse(pyobj.typecode)
         sub2 = pyobj2.Actor
@@ -163,7 +208,6 @@ class DTTestCase(ServiceTestCase):
         sw = SoapWriter()
         sw.serialize(pyobj)
         xml = str(sw)
-        print xml        
         ps = ParsedSoap(xml)
         pyobj2 = ps.Parse(pyobj.typecode)
         sub2 = pyobj2.Actor
